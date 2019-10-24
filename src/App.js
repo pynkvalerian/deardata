84170fbd473502e83b6f8daa20efa2c6c7fee47a
pynkvalerian <pynkvalerian@gmail.com>
2019-10-24 23:56:08 +0800

    initialised repo with line chart and week one data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Bar, Donut, Line} from 'react-roughviz'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <div className="row">
+            <div className="ten columns">
+              <h1>Dear Data by Ping</h1>
+              <Line
+                data='https://raw.githubusercontent.com/jwilber/random_data/master/tweets.csv'
+                title='Line Chart'
+                y='favorites'
+                y2='retweets'
+                y3='tweets'
+                yLabel='Negative emotions'
+                xLabel='Time'
+                colorVar='continent'
+                highlightLabel='country'
+                highlight='red'
+                strokeWidth={3}
+                roughness={1}
+                width={window.innerWidth * .7}
+                labelFontSize='2rem'
+                tooltipFontSize='5rem'
+              />
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
